8df9afce5a42e3a9196225e3e312879cc6400cbf
Alexander Chuprun <achuprun@simtechdev.com>
2016-01-20 11:08:43 +0300

    Chapter 4 bubbles

--- all.js
+++ all.js
@@ -4,20 +4,23 @@ var scores = [60, 50, 60, 58, 54, 54,
               69, 64, 66, 55, 52, 61,
               46, 31, 57, 52, 44, 18,
               41, 53, 55, 61, 51, 44];
+var costs = [.25, .27, .25, .25, .25, .25,
+			 .33, .31, .25, .29, .27, .22,
+			 .31, .25, .25, .33, .21, .25,
+             .25, .25, .28, .25, .24, .22,
+             .20, .25, .30, .25, .24, .25,
+             .25, .25, .27, .25, .26, .29];
 
 function printAndGetHighScore (scores) {
 	var highScore = 0;
-	var output;
-
 	for (var i = 0; i < scores.length; i++) {
-		output = "Bubble solution #" + i + " score: " + scores[i];
-		console.log(output);
 		if (scores[i] > highScore) {
 			highScore = scores[i];
 		}
 	}
 	return highScore;
 }
+
 var highScore = printAndGetHighScore(scores);
 console.log("Bubbles tests: " + scores.length);
 console.log("Highest bubble score: " + highScore);
@@ -35,4 +38,20 @@ function getBestResults (highScore, scores) {
 
 var bestSolutions = getBestResults(highScore, scores);
 console.log("Solutions with the highest score: " + bestSolutions);
+
+function getMostCostEffectiveSolution2(scores, costs, highScore) {
+	var cost = 100;
+	var lowCostIndex;
+
+	for (var i = 0; i < bestSolutions.length; i++) {
+		if (cost > bestSolutions[i]) {
+			lowCostIndex = bestSolutions[i];
+			cost = bestSolutions[i];
+		}
+	}
+	return lowCostIndex;
+}
+
+var marketSolution = getMostCostEffectiveSolution2(scores, costs, highScore);
+console.log("Best coff: " + marketSolution);
 
